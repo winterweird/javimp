0f351ffec767ee36b3eca47c6c1d946915781a50
winterweird <veggitland@hotmail.com>
2016-06-22 03:53:07 +0200

    Update test.sh again

--- test.sh
+++ test.sh
@@ -7,51 +7,53 @@ TESTFILEBACKUP=TestJava.java~
 T1=ArrayList
 T2="Ellipse2D LastOwnerException FileCacheImageInputStream"
 T3="Activity Bundle OnClickListener"
+HL="highlight -O ansi"
+PYGM="pygmentize -g"
 
-printf "TESTING BASIC OUTPUT \n"
+printf "\nTESTING BASIC OUTPUT \n"
 printf "${PY} ${PROGRAMNAME} ${T1}\n\n"
 printf "OUTPUT: \n"
-${PY} ${PROGRAMNAME} ${T1}
+${PY} ${PROGRAMNAME} ${T1} 2>/dev/null
 printf "\n\nTESTING MULTIPLE ARGUMENTS FROM STANDARD LIBRARY \n"
 printf "${PY} ${PROGRAMNAME} ${T2} \n\n"
 printf "OUTPUT: \n"
-${PY} ${PROGRAMNAME} ${T2}
+${PY} ${PROGRAMNAME} ${T2} 2>/dev/null
 printf "\n\nTESTING OUTPUT FOR ANDROID API \n"
 printf "${PY} ${PROGRAMNAME} ${T3} \n\n"
 printf "OUTPUT: \n"
-${PY} ${PROGRAMNAME} ${T3}
+${PY} ${PROGRAMNAME} ${T3} 2>/dev/null
 printf "\n\nTESTING -a OPTION \n"
 printf "${PY} ${PROGRAMNAME} -a ${T3} \n\n"
 printf "OUTPUT: \n"
-${PY} ${PROGRAMNAME} -a ${T3}
+${PY} ${PROGRAMNAME} -a ${T3} 2>/dev/null
 printf "\n\nTESTING -c OPTION \n"
-printf "If successful, you should check the content of your clipboard afterwards \n"
-${PY} ${PROGRAMNAME} -c ${T2}
+printf "If successful, you should check the content of your clipboard afterwards \n\n"
+${PY} ${PROGRAMNAME} -c ${T2} 2>/dev/null
 printf "\n\nTESTING -o OPTION \n"
 printf "${PY} ${PROGRAMNAME} -o ${T3} \n\n"
 printf "CONTENT AFTER: \n"
-${PY} ${PROGRAMNAME} -o ${T3}
+${PY} ${PROGRAMNAME} -o ${T3} 2>/dev/null
 cat import_statements.txt
 rm import_statements.txt
 printf "\n\nTESTING -i OPTION WITH ${TESTFILE} \n"
 printf "${PY} ${PROGRAMNAME} -i ${TESTFILE} \n\n"
 printf "CONTENT BEFORE: \n"
-cat ${TESTFILE}
-printf "\n\nCONTENT AFTER: \n"
-${PY} ${PROGRAMNAME} -i ${TESTFILE}
-cat ${TESTFILE}
+${PYGM} ${TESTFILE} || ${HL} ${TESTFILE} || cat ${TESTFILE}
+printf "\nCONTENT AFTER: \n"
+${PY} ${PROGRAMNAME} -i ${TESTFILE} 2>/dev/null
+${PYGM} ${TESTFILE} || ${HL} ${TESTFILE} || cat ${TESTFILE}
 cp ${TESTFILEBACKUP} ${TESTFILE}
-printf "\n\n\nTESTING -a OPTION WITH OTHER OPTIONS \n\n"
+printf "\n\nTESTING -a OPTION WITH OTHER OPTIONS \n\n"
 printf "${PY} ${PROGRAMNAME} -a -o ${T3} \n\n"
 printf "CONTENT AFTER: \n"
-${PY} ${PROGRAMNAME} -a -o ${T3}
+${PY} ${PROGRAMNAME} -a -o ${T3} 2>/dev/null
 cat import_statements.txt
 rm import_statements.txt
 printf "\n\n${PY} ${PROGRAMNAME} -a -i ${TESTFILE} \n\n"
 printf "CONTENT BEFORE: \n"
-cat ${TESTFILE}
-printf "\n\nCONTENT AFTER: \n"
-${PY} ${PROGRAMNAME} -a -i ${TESTFILE}
-cat ${TESTFILE}
+${PYGM} ${TESTFILE} || ${HL} ${TESTFILE} || cat ${TESTFILE}
+printf "\nCONTENT AFTER: \n"
+${PY} ${PROGRAMNAME} -a -i ${TESTFILE} 2>/dev/null
+${PYGM} ${TESTFILE} || ${HL} ${TESTFILE} || cat ${TESTFILE}
 cp ${TESTFILEBACKUP} ${TESTFILE}
-printf "\n\n\nALL TESTS COMPLETED\n\n"
+printf "\n\nALL TESTS COMPLETED\n\n"
